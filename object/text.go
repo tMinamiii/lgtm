b6628a010cfd235a9b8b844f5fe97d2ff2ba1254
tMinamiii <otherthan.new@gmail.com>
2024-06-22 17:52:38 +0900

    fix: calc fontsize

--- object/text.go
+++ object/text.go
@@ -1,7 +1,6 @@
 package object
 
 import (
-	"bytes"
 	"image"
 	"image/color"
 	"strings"
@@ -79,19 +78,10 @@ func NewText(text string, font Font, messageType MessageType, textColor TextColo
 func (t *Text) FontSize(img image.Image) float64 {
 	imageWidth := img.Bounds().Dx()
 	textLength := len(t.Text.String())
-	// 画像の横幅8割をテキストエリアとする
-	textAreaWidth := float64(imageWidth) * 0.8
-	scale := float64(1)
+	// 画像の横幅9割をテキストエリアとする
+	textAreaWidth := float64(imageWidth) * 0.9
 
-	// フォントに合わせた微調整
-	// 表示された「Mの文字の縦横比」をかけたもの
-	if bytes.Equal(t.Font, NotoSansMono) {
-		scale = float64(6.4) / 5
-	} else if bytes.Equal(t.Font, LINESeedJP) {
-		scale = float64(5) / 5.4
-	}
-
-	return textAreaWidth / float64(textLength) * float64(scale)
+	return textAreaWidth / float64(textLength)
 }
 
 func (t *Text) Point(img image.Image) *Point {
