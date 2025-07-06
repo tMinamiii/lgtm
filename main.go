9a74b294148c7643f44717f0371344c1e114aca2
TmINAMIII <otherthan.new@gmail.com>
2025-07-06 15:33:00 +0900

    feat: use corba

--- main.go
+++ main.go
@@ -1,52 +1,65 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"os"
-	"strings"
 
+	"github.com/spf13/cobra"
 	"github.com/tMinamiii/lgtm/drawer"
 	"github.com/tMinamiii/lgtm/object"
 )
 
-const (
-	flagColor  = "c"
-	flagFont   = "f"
-	flagGopher = "gopher"
-	flagOutput = "o"
+var (
+	color      string
+	fontName   string
+	gopher     bool
+	outputPath string
 )
 
-func main() {
-	color := flag.String("c", "white", "color 'white' or 'black'")
-	fontName := flag.String("f", "sans", "sans, line")
-	gopher := flag.Bool("gopher", false, "embed gopher")
-	output := flag.String("o", "", "output file path")
-	flag.Parse()
+var rootCmd = &cobra.Command{
+	Use:   "lgtm [flags] <image-path>",
+	Short: "Embed 'LGTM' text or gopher image on images",
+	Long: `LGTM is a CLI tool that embeds "LGTM" text on images with customizable colors and fonts.
+It can also embed a gopher image and outputs the result as a JPEG file.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		inputPath := args[0]
 
-	inputPath := path()
-	outputPath := *output
-	if *gopher {
-		d := drawer.NewGopherDrawer()
+		if gopher {
+			d := drawer.NewGopherDrawer()
+			if err := d.Draw(inputPath, outputPath); err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+
+		textColor := object.TextColorWhite
+		if color == "black" {
+			textColor = object.TextColorBlack
+		}
+
+		font := getFont(fontName)
+		main := object.NewText(object.DefaultMainText, font, object.MessageTypeMain, textColor)
+		sub := object.NewText(object.DefaultSubText, font, object.MessageTypeSub, textColor)
+
+		d := drawer.NewTextDrawer(main, sub)
 		if err := d.Draw(inputPath, outputPath); err != nil {
 			log.Fatal(err)
-			os.Exit(1)
 		}
-		return
-	}
+	},
+}
 
-	textColor := object.TextColorWhite
-	if *color == "black" {
-		textColor = object.TextColorBlack
-	}
+func init() {
+	rootCmd.Flags().StringVarP(&color, "color", "c", "white", "text color: 'white' or 'black'")
+	rootCmd.Flags().StringVarP(&fontName, "font", "f", "sans", "font type: 'sans' or 'line'")
+	rootCmd.Flags().BoolVar(&gopher, "gopher", false, "embed gopher image instead of text")
+	rootCmd.Flags().StringVarP(&outputPath, "output", "o", "", "output file path")
+}
 
-	font := getFont(*fontName)
-	main := object.NewText(object.DefaultMainText, font, object.MessageTypeMain, textColor)
-	sub := object.NewText(object.DefaultSubText, font, object.MessageTypeSub, textColor)
-
-	d := drawer.NewTextDrawer(main, sub)
-	if err := d.Draw(inputPath, outputPath); err != nil {
-		log.Fatal(err)
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
@@ -60,19 +73,4 @@ func getFont(fontName string) object.Font {
 	default:
 		return object.NotoSansMono
 	}
-}
-
-func isNotBoolFlag(arg string) bool {
-	return strings.HasPrefix(arg, "-") && !strings.HasSuffix(arg, flagGopher)
-}
-
-func path() string {
-	last := flag.NArg() - 1
-	path := flag.Arg(last)
-	prev := flag.Arg(last - 1)
-	if path == "" || isNotBoolFlag(prev) {
-		log.Fatal("no image path")
-		os.Exit(1)
-	}
-	return path
 }
